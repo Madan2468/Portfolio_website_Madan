d37e12cf764551f496be60f1cb369a0028affbc0
Madan2468 <madanjha2468@gmail.com>
2025-01-10 00:59:18 +0530

    added speed insights

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"; // Add the import statement for SpeedInsights
 
 function Layout() {
   return (
@@ -11,6 +12,8 @@ function Layout() {
       <Outlet />
       <Footer />
       <Analytics /> {/* This component will track analytics for all pages */}
+      <SpeedInsights />{" "}
+      {/* This component will track speed insights for all pages */}
     </>
   );
 }
